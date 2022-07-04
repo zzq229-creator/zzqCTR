e53c08709b4ccdddc6247677b58fdd6774ce856f
zzq229-creator <zzq229@gmail.com>
2022-07-04 16:26:55 +0800

    init
    fft for deepfm autoint dcn

---
+++ fuxictr/pytorch/layers/filterlayer.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+
+
+class FilterLayer(nn.Module):
+    def __init__(self, alpha_fft, max_seq_length, hidden_size, drouput):
+        super(FilterLayer, self).__init__()
+        # 做完self-attention 做一个前馈全连接 LayerNorm 输出
+        self.complex_weight = nn.Parameter(torch.randn(1, max_seq_length // 2 + 1, hidden_size, 2, dtype=torch.float32) * alpha_fft)
+        self.out_dropout = nn.Dropout(drouput)
+        self.LayerNorm = LayerNorm(hidden_size, eps=1e-12)
+
+    def forward(self, input_tensor):
+        # [batch, seq_len, hidden]
+        # sequence_emb_fft = torch.rfft(input_tensor, 2, onesided=False)  # [:, :, :, 0]
+        # sequence_emb_fft = torch.fft(sequence_emb_fft.transpose(1, 2), 2)[:, :, :, 0].transpose(1, 2)
+        batch, seq_len, hidden = input_tensor.shape
+        x = torch.fft.rfft(input_tensor, dim=1, norm='ortho')
+        weight = torch.view_as_complex(self.complex_weight)
+        x = x * weight
+        sequence_emb_fft = torch.fft.irfft(x, n=seq_len, dim=1, norm='ortho')
+        hidden_states = self.out_dropout(sequence_emb_fft)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
