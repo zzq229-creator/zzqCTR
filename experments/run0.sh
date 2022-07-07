24ff30d0df1c11e012a1741fb0a805ad0979f51d
zzq229-creator <zzq229@gmail.com>
2022-07-07 19:34:19 +0800

    DCN pool

--- experments/run0.sh
+++ experments/run0.sh
@@ -1,6 +1,9 @@
-python run_csv_config.py --gpu 0 --experiment_id DNN_base_avazu
 python run_csv_config.py --gpu 0 --experiment_id DNN_fft_avazu
 python run_csv_config.py --gpu 0 --experiment_id DNN_pool_avazu
+python run_csv_config.py --gpu 0 --experiment_id DNN_base_avazu
 
-python run_csv_config.py --gpu 0 --experiment_id AutoInt_base_avazu
 python run_csv_config.py --gpu 0 --experiment_id AutoInt_fft_avazu
+python run_csv_config.py --gpu 0 --experiment_id AutoInt_base_avazu
+python run_csv_config.py --gpu 0 --experiment_id DCN_pool_avazu
+python run_csv_config.py --gpu 0 --experiment_id DCN_base_avazu
+
